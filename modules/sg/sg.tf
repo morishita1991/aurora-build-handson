5950ed909ed8b14d09ccbe6924b817ae7c6d2d6e
Hideki Morishita <h.morishita@rescuenow.co.jp>
2025-02-25 15:46:26 +0900

    セキュリティグループを追加

--- modules/sg/sg.tf
+++ modules/sg/sg.tf
@@ -1,8 +1,17 @@
 resource "aws_security_group" "aurora_sg" {
   description = "Security group for Aurora DB"
-  name        = "aurora-security-group"
+  name        = "aurora-sg"
   vpc_id      = var.vpc_id
   tags = {
     Name = "aurora-sg"
+  }
+}
+
+resource "aws_security_group" "aurora_sql_instance_sg" {
+  description = "Security group for allowing SQL access to Aurora"
+  name        = "aurora-sql-access-sg"
+  vpc_id      = var.vpc_id
+  tags = {
+    Name = "aurora-sql-access-sg"
   }
 }
--- modules/sg/sg_rule.tf
+++ modules/sg/sg_rule.tf
@@ -1,18 +1,28 @@
 # Aurora
-resource "aws_security_group_rule" "aurora_mysql" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
+resource "aws_security_group_rule" "ingress_aurora" {
+  from_port         = "3306"
   protocol         = "tcp"
   security_group_id = aws_security_group.aurora_sg.id
-  cidr_blocks       = var.is_osaka ? ["10.0.0.0/16"] : ["10.1.0.0/16"]  # TODO: 必要に応じて変更
+  source_security_group_id = aws_security_group.aurora_sql_instance_sg.id
+  to_port           = "3306"
+  type              = "ingress"
 }
 
-resource "aws_security_group_rule" "aurora_outbound" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol         = "-1"
+resource "aws_security_group_rule" "egress_aurora" {
+  from_port         = "0"
+  protocol          = "-1"
   security_group_id = aws_security_group.aurora_sg.id
   cidr_blocks       = ["0.0.0.0/0"]
+  to_port           = "0"
+  type              = "egress"
+}
+
+# Aurora SQL Instance
+resource "aws_security_group_rule" "egress_aurora_sql_instance" {
+  from_port         = "0"
+  protocol          = "-1"
+  security_group_id = aws_security_group.aurora_sql_instance_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  to_port           = "0"
+  type              = "egress"
 }
